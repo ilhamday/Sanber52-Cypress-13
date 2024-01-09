ff2a08001a24dfc82835fbaa29c6f5c5627ecc37
wahidahizmi <wahidahizadh@gmail.com>
2024-01-09 18:03:19 +0700

    wahidah update file edit address

--- cypress/e2e/3-web-test/magento/edit-eddress.cy.js
+++ cypress/e2e/3-web-test/magento/edit-eddress.cy.js
@@ -1,30 +1,27 @@
 describe('Edit Address', () => {
-  beforeEach(() => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/')
-  })
+  //beforeEach(() => {
+    //cy.visit('https://magento.softwaretestingboard.com/customer/account/')
+  //})
   
   it('Login My Account Billing', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/')
     cy.get('#email').type('wahidahizadh@gmail.com')
     cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('Wahidah28*')
     cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click()
     
-    //link edit address 
+    //edit address account billing
     cy.get('.box-billing-address > .box-actions > .action > span').click()
   })
 
   it('Edit Contact Information', () =>{
-    cy.get('#firstname')
-      .clear()
-      .type('Wahidah Izmi')
-
-    cy.get('#lastname')
-      .clear()
-      .type('Addinah')
-      
+    
+    cy.get('#firstname').type('Wahidah Izmi')
+    cy.get('#lastname').type('Addinah')
     cy.get('#telephone').type('082276956300')
   })
 
   it('Edit Addres', () =>{
+    
     cy.get('#street_1').type('Pematang Siantar')
     cy.get('#city').type('Medan')
     cy.get('#region_id').click()
